c72716f034ff454af555e085ced5de9420e48300
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2015-11-26 07:01:37 +0100

    Better (readable) setup for building and watching javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,19 @@ var gulp = require('gulp'),
     watch = require('gulp-watch'),
     batch = require('gulp-batch'),
     watchify = require('watchify'),
-    util = require('gulp-util');
+    util = require('gulp-util'),
+    _ = require('lodash');
 
+/**
+ * @type {Object}
+ */
 var paths = {
     js: 'src/js/**/*.js',
     js_entry: 'src/js/index.js'
 };
 
 /**
- * @param {Error} error
+ * @param {Object} error
  */
 var handleError = function (error) {
     util.log(
@@ -22,13 +26,17 @@ var handleError = function (error) {
         '\n\nin: ' + error.filename + ' (' + error.pos + ')\n\n',
         error.codeFrame + '\n\n'
     );
+
+    this.emit('end');
 };
 
 /**
  * @param {Boolean} doWatch
- * @param {Function} cb
+ * @param {Function} onFinishedCallback
  */
-var doBuild = function (doWatch, cb) {
+var build = function (doWatch, onFinishedCallback) {
+    util.log(' --> building..');
+
     var bundler = browserify({
         entries: paths.js_entry,
         extensions: ['.js'],
@@ -39,55 +47,82 @@ var doBuild = function (doWatch, cb) {
     });
 
     if (doWatch) {
-        // use watchify instead of gulp.watch because it is quite a lost faster!
+        // use watchify instead of gulp.watch because it is quite a lost faster!!
         bundler = watchify(bundler);
     }
+
+    transformCode(bundler);
+
+    bundler.on('update', function () {
+        transformCode(bundler);
+
+        bundle(bundler, null);
+    });
+
+    return bundle(bundler, onFinishedCallback);
+};
+
+/**
+ * @param {Object} bundler
+ * @param {Function} onFinishedCallback
+ *
+ * @returns {Object}
+ */
+var bundle = function (bundler, onFinishedCallback) {
+    util.log(' --> bundling..');
+
+    var stream = bundler.bundle();
+
+    // handle events, as the streaming is asynchronous we want some feedback about
+    // the progress
+    stream
+        .on('error', handleError)
+        .on('end', function () {
+            util.log(' --> done..\n');
+            util.log(util.colors.green('-----------------------') + '\n');
+
+            if (_.isFunction(onFinishedCallback)) {
+                onFinishedCallback();
+            }
+        });
+
+    // bundle files in bundle.js in build folder
+    stream
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('build/js'));
+
+    return stream;
+};
+
+/**
+ * @param {Object} bundler
+ *
+ * @return {Object}
+ */
+var transformCode = function (bundler) {
+    util.log(' --> transforming..');
 
     bundler
         .transform(babelify, {
             presets: ['es2015', 'react']
         });
 
-    var rebundle = function () {
-        console.log(' --> detected changes..');
-        console.log(' --> building..');
-
-        var stream = bundler.bundle();
-
-        // handle events
-        stream
-            .on('error', handleError)
-            .on('end', function () {
-                console.log(' --> done..\n');
-                console.log('-----------------------\n');
-            });
-
-        // pipe to bundle.js in build folder
-        stream
-            .pipe(source('bundle.js'))
-            .pipe(gulp.dest('build/js'));
-
-        return stream;
-    }
-
-    bundler.on('update', rebundle);
-
-    return rebundle();
-}
+    return bundler;
+};
 
 /**
  * Takes javascript, convers ECMA5 and React code to general and supported javascript,
  * and bundles it in one file
  */
 gulp.task('build', function (cb) {
-    doBuild(false, cb);
+    build(false, cb);
 });
 
 /**
  * Watches for changes and rebuilds when one is detected
  */
 gulp.task('watch', function (cb) {
-    doBuild(true, cb);
+    build(true);
 });
 
 gulp.task('default', ['build']);
